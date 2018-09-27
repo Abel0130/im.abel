434f2fee82963bc32d391c591aa6e68ee5063d94
Abel <abel0130@163.com>
2018-09-27 17:51:50 +0800

    项目初始化

---
+++ sinablog-server/src/main/java/me/abel/sinablog/server/SinablogApplication.java
@@ -0,0 +1,18 @@
+package me.abel.sinablog.server;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+/**
+ * @description:
+ * @author: able.li
+ * @create: 2018/9/27 10:37
+ */
+@SpringBootApplication
+public class SinablogApplication {
+
+    public static void main(String[] args){
+        SpringApplication.run(SinablogApplication.class, args);
+    }
+
+}
